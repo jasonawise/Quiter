0a5c26a73b0bc13e6182b21a9306358219c78f91
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-30 19:00:07 -0400

    Initial Commit

---
+++ Quiter/QuiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiterApp.swift
+//  Quiter
+//
+//  Created by Jason Wise on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct QuiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
