aa256cf89c41fd929fc44d1eefcc17347c366e07
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-30 21:56:03 -0400

    adding new field
    added new field to get name of what they want to quit

--- Quiter/AddQuiterView.swift
+++ Quiter/AddQuiterView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct AddQuiterView: View {
     @State private var stopedDate = Date()
     @State private var today = Date()
+    @State private var quitterName: String = ""
  
     var body: some View {
-        VStack {
+        VStack() {
+            TextField("What did / do you want to stop?", text: $quitterName)
+                .font(.title2)
+                .padding()
             Text("When did you stop it?")
                 .font(.title2)
                 .fontWeight(.light)
